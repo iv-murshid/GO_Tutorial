cde8d0010fe47ab616b862b00a3251f35962397b
Murshid Raja <murshidraja652@example.com>
2024-06-24 20:11:45 +0530

    2: Values

---
+++ values/values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("strings "+"are "+"added")
+	fmt.Println("2 + 3 = " , 2+3)
+	fmt.Println("7.0/2.0 =", 7.0/2.0)
+	fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
