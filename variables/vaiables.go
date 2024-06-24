fa4f64e184672e9b99073fea08b6c811d12239dc
Murshid Raja <murshidraja652@example.com>
2024-06-24 23:33:33 +0530

    3: variables

---
+++ variables/vaiables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a = "ini"
+    fmt.Println(a)
+
+    var b, c int = 3,4
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+
+    f := "apple"
+    fmt.Println(f)
+}
